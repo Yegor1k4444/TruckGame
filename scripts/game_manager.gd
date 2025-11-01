e9a04fa5793b550b161e3157d3f615012bfb645f
Yegor <160131141+Yegor1k4444@users.noreply.github.com>
2025-11-02 01:59:28 +0200

    Add some models, sound effect, radio, model for track

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -12,9 +12,54 @@ var current_mission = Missions.GO_TO_WAREHOUSE_B
 
 # Посилання на наш UI Label, щоб змінювати текст
 @export var objective_label: Label
+@export var radio_player : AudioStreamPlayer
+@export var tutorial_label: Label
+
+enum TutorialState {
+	SHOW_MOVE,      # Показуємо "Натисніть W"
+	SHOW_STEER,     # Показуємо "Натисніть A/D"
+	SHOW_CAMERA,    # Показуємо "Натисніть Space/C"
+	DONE            # Навчання завершено
+}
+
+# Змінна, що зберігає наш поточний етап
+var current_tutorial_state = TutorialState.SHOW_MOVE
 
 func _ready():
 	update_ui_objective()
+	
+	radio_player.playing = $"/root/PlayScrene/CanvasLayer/CheckBox".button_pressed # <-- Замініть шлях на правильний до вашого CheckBox
+	
+	# -- ДОДАЙТЕ ЦІ РЯДКИ --
+	tutorial_label.text = "Натисніть [W], щоб почати рух"
+	tutorial_label.visible = true
+
+func _process(delta: float) -> void:
+	if current_tutorial_state == TutorialState.DONE:
+		return
+
+	match current_tutorial_state:
+		
+		TutorialState.SHOW_MOVE:
+			# Якщо ми на етапі "Рух" і гравець натиснув "вперед"
+			if Input.is_action_just_pressed("move_forward"):
+				current_tutorial_state = TutorialState.SHOW_STEER
+				tutorial_label.text = "Використовуйте [A] та [D] для поворотів"
+			
+		TutorialState.SHOW_STEER:
+			# Якщо ми на етапі "Поворот" і гравець натиснув вліво АБО вправо
+			if Input.is_action_just_pressed("steer_left") or Input.is_action_just_pressed("steer_right"):
+				current_tutorial_state = TutorialState.SHOW_CAMERA
+				tutorial_label.text = "Натисніть [C], щоб змінити вид камери" 
+				
+		TutorialState.SHOW_CAMERA:
+			# Якщо ми на етапі "Камера" і гравець натиснув 'switch_camera'
+			if Input.is_action_just_pressed("switch_camera"):
+				# Завершуємо навчання
+				current_tutorial_state = TutorialState.DONE
+				# Ховаємо лейбл
+				tutorial_label.visible = false
+
 
 # Функція для оновлення тексту завдання на екрані
 func update_ui_objective():
@@ -49,3 +94,12 @@ func _on_factory_c_reached(body):
 			print("Друга місія виконана!")
 			current_mission = Missions.ALL_COMPLETE
 			update_ui_objective()
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		# Галочка є - вмикаємо музику (точніше, вимикаємо тишу)
+		radio_player.playing = true
+	else:
+		# Галочки немає - вимикаємо музику (вмикаємо тишу)
+		radio_player.playing = false
--- scripts/truck.gd
+++ scripts/truck.gd
@@ -1,10 +1,12 @@
 extends VehicleBody3D
 @onready var tps_camera = $SpringArm3D/ThirdPersonCamera
 @onready var fps_camera = $FirstPersonCamera
+@onready var engine_sound = $EngineSound
 
 # Налаштування, які можна змінювати в інспекторі
 @export var engine_power: float = 120.0
 @export var steer_angle: float = 0.6
+
 
 func _physics_process(delta):
 	# Керування кермом
@@ -14,6 +16,14 @@ func _physics_process(delta):
 	# Керування газом і гальмом
 	var gas_input = Input.get_axis("move_backward", "move_forward")
 	engine_force = gas_input * engine_power
+	
+	# Логіка гучності двигуна
+	if gas_input != 0:
+		# Коли газ натиснутий, робимо звук гучнішим (0 dB - повна гучність)
+		engine_sound.volume_db = -10
+	else:
+		# Коли стоїмо, робимо звук дуже тихим (-20 dB - майже нечутно)
+		engine_sound.volume_db = -40
 
 func _unhandled_input(event):
 	# Перевіряємо, чи була натиснута клавіша "C" (або будь-яка інша)
